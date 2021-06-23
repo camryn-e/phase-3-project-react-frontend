35c518a0cb741c32ba36d94534336457ebf04eae
camryn williams <williams.camryn.e@gmail.com>
2021-06-23 10:08:11 -0400

    added welcome message to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div>
         <h1> Pride Event Database </h1>
+        <p>Welcome to the worldwide pride events database! This app allows users to create, view, and cancel LGBT pride events! Happy Pride Month!</p>
         </div>
     )
 }
